a9cf0bf4f1136ef51b8a807f6d89517ab7c7354f
kawagh <kawagh.dev@gmail.com>
2024-12-07 23:13:28 +0900

    add my git aliases to zabrze

--- makelinks.sh
+++ makelinks.sh
@@ -5,3 +5,5 @@ ln -sfv ${PWD}/.ideavimrc ~/.ideavimrc
 ln -sfv ${PWD}/.gitconfig ~/.gitconfig
 ln -sfv ${PWD}/.tigrc ~/.tigrc
 ln -sfv ${PWD}/.zshrc ~/.zshrc
+mkdir -p ~/.config/zabrze
+ln -sfv ${PWD}/.config/zabrze/config.yaml ~/.config/zabrze/config.yaml
