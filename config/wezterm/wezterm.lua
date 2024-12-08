7801563efcb6970b3cdee5da2708fbc3a58aec1e
kawagh <kawagh.dev@gmail.com>
2024-12-08 11:47:12 +0900

    modify tabbar

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -6,6 +6,9 @@ local config = wezterm.config_builder()
 config.font_size = 13.0
 config.color_scheme = "Ubuntu"
 config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
 config.leader = { key = "q", mods = "CTRL" }
 config.keys = {
 	{
